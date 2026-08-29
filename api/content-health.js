68cc02af21e5fe402f9f0dfeaae905e7a4a100fe
Sauravssoni <85058209+Sauravssoni@users.noreply.github.com>
2026-08-29 15:05:17 +0530

    api: add content health fixture

---
+++ api/content-health.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+  res.status(200).json({ prototype_truth: 'SYNTHETIC_KPI', pagesReviewed: 412, staleOver90Days: 12, supersededCirculars: 3, brokenExternalLinks: 2, bilingualParityPercent: 96, ownerMetadataCoveragePercent: 91 });
+};
